a6a0483223a92bbb5307a22a0e90994c988017b2
tnsChiller <starkster@Ilhans-MacBook-Air.local>
2024-05-08 20:43:42 +0100

    -

--- MakeParameterSetsNoSQL.py
+++ MakeParameterSetsNoSQL.py
@@ -1,20 +1,27 @@
 import numpy as np
 import Utilities as util
 import pickle
-import uuid
 
 with open("lastHist.pickle", "rb") as f:
-    hist = pickle.load(f)
+    hist0 = pickle.load(f)
     
+vSplit = 0.2
+hist = hist0[:, :int(hist0.shape[1] * (1 - vSplit)), :]
+histV = hist0[:, int(hist0.shape[1] * (1 - vSplit)):, :]
 metrics = util.GetMetrics(hist)
-popSize, gens = 1000, 100
+popSize, pieceSize, gens = 5000, 200, 100
+# popSize,  gens = 800, 150
 pop = util.StartMetricPopulation(metrics, popSize)
 (pop, gains) = util.EvolvePopulation(hist, gens, pop)
+(pop, gainsV) = util.EvolvePopulation(histV, 0, pop)
 
-for i in range(20):
-    if i == 0:
-        idx = int(popSize * 0.2) - 1
-    else:
-        idx = np.random.randint(0, popSize * 0.2)
-        
-    util.SaveParameterSet(pop[idx], gains[idx], str(uuid.uuid4()))
+res = np.concatenate((gainsV.reshape(popSize, 1),
+                      gains.reshape(popSize, 1),
+                      pop), axis = 1)
+res = res[res[:, 0].argsort()]
+
+selection = []
+for idx in range(popSize - 20, popSize):
+    if res[idx, 1] > 2 and res[idx, 0] > 2:
+        selection.append([res[idx, 1], res[idx, 0], res[idx, 2:]])
+        util.SaveParameterSet(res[idx, 2:], "-", res[idx, 1], res[idx, 0])
