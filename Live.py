b896484f2395fd721f053ac92ce4e9adcd26a434
tnsChiller <starkster@Ilhans-MacBook-Air.local>
2024-05-25 19:41:01 +0100

    -

--- Live.py
+++ Live.py
@@ -6,27 +6,33 @@ pSets = util.LoadFile("pSets")
 print("\nStarting ...")
 go = True
 while True:
-    clock = extt.GetClock()
-    if clock.is_open:
-        if util.GetGoTime(31, 0):
-        # if True:
-            if go:
-                yfd = extt.GetYFD()
-                metrics = util.GetLiveMetrics(yfd)
-                moves = util.GetMoves(pSets, metrics, playList)
-                orderList = util.CreateOrderList(moves, yfd, pSets)
-                extt.ExecuteOrders(orderList, playList)
-                go = False
+    try:
+        clock = extt.GetClock()
+        if clock.is_open:
+            if util.GetGoTime(31, 0):
+            # if True:
+                if go:
+                    yfd = extt.GetYFD()
+                    metrics = util.GetLiveMetrics(yfd)
+                    moves = util.GetMoves(pSets, metrics, playList)
+                    orderList = util.CreateOrderList(moves, yfd, pSets)
+                    extt.ExecuteOrders(orderList, playList)
+                    go = False
+                    
+            else:
+                go = True
                 
         else:
-            go = True
+            print("Market is closed.")
+            print(f"Time: {extt.GetTime()}")
+        
+        # util.RunSummary()
+        extt.RunPositionSummary()
+        for cycle in range(12):
+            util.WaitCycle()
             
-    else:
-        print("Market is closed.")
-        print(f"Time: {extt.GetTime()}")
-    
-    # util.RunSummary()
-    extt.RunPositionSummary()
-    for cycle in range(12):
-        util.WaitCycle()
+    except:
+        print("Exception: waiting 1 min")
+        for cycle in range(12):
+            util.WaitCycle()
         
